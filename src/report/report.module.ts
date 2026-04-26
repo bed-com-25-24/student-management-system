f9e2e1fcce02fba4f1f76a15b94838d077f01d23
BSC-COM-37-24 <bsc-com-37-24@unima.ac.mw>
2026-04-26 18:24:39 +0200

    Partlt Worked on my Report module,service and dto

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { Report } from './entities/report.entity';
 
 @Module({
   providers: [ReportService],
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'; 
+import { InjectRepository } from '@nestjs/typeorm'; 
+import { Repository } from 'typeorm'; 
+import { Report } from './entities/report.entity'; 
+mport { CreateRepertDto } from './dto/create-report.dto'; 
+mport { UpdateReportDto } from './dto/update-report.dto'; 
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private reportRepository: Repository<Report>,
+  ) {}
+
+}
