5d01c4dea3b3d38d96526ec714133c4690e1a1f1
BSC-COM-37-24 <bsc-com-37-24@unima.ac.mw>
2026-05-07 09:21:24 +0200

    feat: add ApiProperty decorators to CreateReportDto for improved Swagger documentation

--- src/report/dto/create-report.dto.ts
+++ src/report/dto/create-report.dto.ts
@@ -1,44 +1,53 @@
 // dto/create-report.dto.ts
 import { IsNumber, IsString,  IsNotEmpty, IsOptional, IsDate, Min, Max  } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateReportDto {
   @IsNumber()
   @IsNotEmpty()
   @Min(0)
+  @ApiProperty()
   total!: number;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   term!: string;
 
   @IsNumber()
   @IsNotEmpty()
   @Min(0)
   @Max(100)
+  @ApiProperty()
   average!: number;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   studentId!: number;
 
   @IsNumber()
   @IsNotEmpty()
   @Min(0)
   @Max(100)
+  @ApiProperty()
   grade!: number;
 
   @IsNumber()
   @IsNotEmpty()
   @Min(1)
+  @ApiProperty()
   rank!: number;
 
   @IsOptional()
    @Type(() => Date)
   @IsDate()
+ @ApiProperty({ required: false }) 
   generatedAt?: Date;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({ required: false })
   classId?: number;
 }
